b6d9590760ab9647eb682fec42bfe814c8ca53ab
Romina Huk <rominahuk@gmail.com>
2026-08-29 20:38:35 -0300

    feat: se actualiza codigo para la migracion a Angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
 import { QuienesSomos } from './pages/quienes-somos/quienes-somos';
 import { Registro } from './pages/auth/registro/registro';
 import { Inicio } from './pages/dashboard-admin/inicio/inicio';
 import { Layout } from './pages/dashboard-admin/layout/layout';
 
 export const routes: Routes = [
-
+  { path: '', component: Home, title: 'Inicio',},
   { path: 'quienes-somos', component: QuienesSomos },
   { path: 'registro', component: Registro },
 
