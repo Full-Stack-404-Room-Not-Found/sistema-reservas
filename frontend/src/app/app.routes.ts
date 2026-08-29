c6ab8616ea008a4bfd2286c4f18908d21c76e751
FernandoMoyano2121 <FernandoMoyano21@gmail.com>
2026-08-29 13:16:51 -0300

    feat: Colocar path y component registro dentro de app.routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { QuienesSomos } from './pages/quienes-somos/quienes-somos';
+import { Registro } from './pages/auth/registro/registro';
 
 export const routes: Routes = [
-    {path: 'quienes-somos', component:QuienesSomos}
-
+  { path: 'quienes-somos', component: QuienesSomos },
+  { path: 'registro', component: Registro },
 ];
