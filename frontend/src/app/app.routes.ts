d4a9b45c1b1bd9b69524a86ac278247b614361be
Nanu <nancy.morales.387.06@gmail.com>
2026-08-30 13:35:18 -0300

    feat: ruteo de inicio

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,14 +4,20 @@ import { Registro } from './pages/auth/registro/registro';
 import { Inicio } from './pages/dashboard-admin/inicio/inicio';
 import { Layout } from './pages/dashboard-admin/layout/layout';
 import { Login } from './pages/auth/login/login';
+import { ConfirmarReserva } from './pages/dashboard-usuario/confirmar-reserva/confirmar-reserva';
+import { HistorialReservas} from './pages/dashboard-usuario/historial-reservas/historial-reservas'; 
+import { InicioUsuario } from './pages/dashboard-usuario/inicio/inicio';
+import { ReservarHabitacion } from './pages/dashboard-usuario/reservar-habitacion/reservar-habitacion';
 
 export const routes: Routes = [
 
   { path: 'quienes-somos', component: QuienesSomos },
   { path: 'registro', component: Registro },
   {path: "login", component: Login},
-
-
+  {path: "confirmar-reserva", component: ConfirmarReserva},
+  {path: "historial-reservas", component: HistorialReservas},
+  {path: "inicio-usuario", component: InicioUsuario},
+  {path: "reservar-habitacion", component: ReservarHabitacion},
   {
     path: 'dashboard-admin',
     component: Layout,
