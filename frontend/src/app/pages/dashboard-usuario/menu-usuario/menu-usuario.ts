a7dce5f3829e66d29cb4cbb1aaa4e024e6a03943
Nanu <nancy.morales.387.06@gmail.com>
2026-08-30 13:47:20 -0300

    feat: componente menu-usuario

---
+++ frontend/src/app/pages/dashboard-usuario/menu-usuario/menu-usuario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-menu-usuario',
+  styleUrl: './menu-usuario.css',
+  templateUrl: './menu-usuario.html',
+})
+export class MenuUsuario {}
