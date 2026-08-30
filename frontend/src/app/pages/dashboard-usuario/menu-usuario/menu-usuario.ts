3e44d6e9a0b98e8226ccc69f19ad9c1ffc6e8cd6
Nanu <nancy.morales.387.06@gmail.com>
2026-08-30 14:18:31 -0300

    Feat: Elimino Componente Menu-Usuario, agrego Reservas al menu

--- frontend/src/app/pages/dashboard-usuario/menu-usuario/menu-usuario.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-menu-usuario',
-  styleUrl: './menu-usuario.css',
-  templateUrl: './menu-usuario.html',
-})
-export class MenuUsuario {}
