350e60114ab87083911a577d4c69326c5403e9ef
lore376 <lorenapereyra376@gmail.com>
2026-08-29 22:45:25 -0300

    feat: agregar detalle y crear habitacion
     Agregar componente DetalleHabitacion
    - Agregar componente CrearHabitacion
    - Actualizar rutas en app.routes.ts
    - Implementar navegación entre componentes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,12 +3,13 @@ import { QuienesSomos } from './pages/quienes-somos/quienes-somos';
 import { Registro } from './pages/auth/registro/registro';
 import { Inicio } from './pages/dashboard-admin/inicio/inicio';
 import { Layout } from './pages/dashboard-admin/layout/layout';
+import { DetalleHabitacion } from './pages/dashboard-admin/detalle-habitacion/detalle-habitacion';
+import { CrearHabitacion } from './pages/dashboard-admin/crear-habitacion/crear-habitacion';
 
 export const routes: Routes = [
 
   { path: 'quienes-somos', component: QuienesSomos },
   { path: 'registro', component: Registro },
-
 
   {
     path: 'dashboard-admin',
@@ -16,7 +17,16 @@ export const routes: Routes = [
     children: [
       { path: '', redirectTo: 'inicio', pathMatch: 'full' },
       { path: 'inicio', component: Inicio },
+
+
+      { path: 'detalle-habitacion/:id', component: DetalleHabitacion },
+      { path: 'crear-habitacion/:id', component: CrearHabitacion },
     ]
   },
 
+
+  { path: '', redirectTo: 'quienes-somos', pathMatch: 'full' },
+
+
+  { path: '**', redirectTo: 'dashboard-admin/inicio' }
 ];
