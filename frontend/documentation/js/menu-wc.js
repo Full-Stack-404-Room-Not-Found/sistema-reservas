8dc4da8ca4f0c758a5e1fb39d796025a6c410b0c
FernandoAMoyano <fernandomoyano21@gmail.com>
2026-09-20 12:23:19 -0300

    fix(main): restaurar lo que el revert del PR #59 saco de main

---
+++ frontend/documentation/js/menu-wc.js
@@ -0,0 +1,146 @@
+'use strict';
+
+customElements.define('compodoc-menu', class extends HTMLElement {
+    constructor() {
+        super();
+        this.isNormalMode = this.getAttribute('mode') === 'normal';
+    }
+
+    connectedCallback() {
+        this.render(this.isNormalMode);
+    }
+
+    render(isNormalMode) {
+        let tp = lithtml.html(`
+        <nav>
+            <ul class="list">
+                <li class="title">
+                    <a href="index.html" data-type="index-link">frontend documentation</a>
+                </li>
+
+                <li class="divider"></li>
+                ${ isNormalMode ? `<div id="book-search-input" role="search">
+    <input type="text" placeholder="Type to search">
+    <button type="button"
+        class="search-input-clear"
+        aria-label="Clear search"
+        data-search-input-clear>&times;</button>
+</div>
+` : '' }
+                <li class="chapter">
+                    <a data-type="chapter-link" href="index.html"><span class="icon ion-ios-home"></span>Getting started</a>
+                    <ul class="links">
+                                <li class="link">
+                                    <a href="overview.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-keypad"></span>Overview
+                                    </a>
+                                </li>
+
+                            <li class="link">
+                                <a href="index.html" data-type="chapter-link">
+                                    <span class="icon ion-ios-paper"></span>
+                                        README
+                                </a>
+                            </li>
+                                <li class="link">
+                                    <a href="dependencies.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-list"></span>Dependencies
+                                    </a>
+                                </li>
+                                <li class="link">
+                                    <a href="properties.html" data-type="chapter-link">
+                                        <span class="icon ion-ios-apps"></span>Properties
+                                    </a>
+                                </li>
+
+                    </ul>
+                </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#components-links"' :
+                            'data-bs-target="#xs-components-links"' }>
+                            <span class="icon ion-md-cog"></span>
+                            <span>Components</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="components-links"' : 'id="xs-components-links"' }>
+                            <li class="link">
+                                <a href="components/App.html" data-type="entity-link" >App</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ConfirmacionReserva.html" data-type="entity-link" >ConfirmacionReserva</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/CrearHabitacion.html" data-type="entity-link" >CrearHabitacion</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/DashboardAdminInicio.html" data-type="entity-link" >DashboardAdminInicio</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/DashboardAdminLayout.html" data-type="entity-link" >DashboardAdminLayout</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/DashboardUsuarioInicio.html" data-type="entity-link" >DashboardUsuarioInicio</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/DashboardUsuarioLayout.html" data-type="entity-link" >DashboardUsuarioLayout</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/DetalleHabitacion.html" data-type="entity-link" >DetalleHabitacion</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/HistorialReservas.html" data-type="entity-link" >HistorialReservas</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Home.html" data-type="entity-link" >Home</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Login.html" data-type="entity-link" >Login</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/NotFound.html" data-type="entity-link" >NotFound</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/QuienesSomos.html" data-type="entity-link" >QuienesSomos</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/Registro.html" data-type="entity-link" >Registro</a>
+                            </li>
+                            <li class="link">
+                                <a href="components/ReservarHabitacion.html" data-type="entity-link" >ReservarHabitacion</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="chapter">
+                        <div class="simple menu-toggler" data-bs-toggle="collapse" ${ isNormalMode ? 'data-bs-target="#miscellaneous-links"'
+                            : 'data-bs-target="#xs-miscellaneous-links"' }>
+                            <span class="icon ion-ios-cube"></span>
+                            <span>Miscellaneous</span>
+                            <span class="icon ion-ios-arrow-down"></span>
+                        </div>
+                        <ul class="links collapse " ${ isNormalMode ? 'id="miscellaneous-links"' : 'id="xs-miscellaneous-links"' }>
+                            <li class="link">
+                                <a href="miscellaneous/functions.html" data-type="entity-link">Functions</a>
+                            </li>
+                            <li class="link">
+                                <a href="miscellaneous/variables.html" data-type="entity-link">Variables</a>
+                            </li>
+                        </ul>
+                    </li>
+                        <li class="chapter">
+                            <a data-type="chapter-link" href="routes.html"><span class="icon ion-ios-git-branch"></span>Routes</a>
+                        </li>
+                    <li class="chapter">
+                        <a data-type="chapter-link" href="coverage.html"><span class="icon ion-ios-stats"></span>Documentation coverage</a>
+                    </li>
+                    <li class="divider"></li>
+                    <li class="copyright">
+                        Documentation generated using <a href="https://compodoc.app/" target="_blank" rel="noopener noreferrer">
+                            <img data-src="images/compodoc-vectorise.png" class="img-responsive" data-type="compodoc-logo">
+                        </a>
+                    </li>
+            </ul>
+        </nav>
+        `);
+        this.innerHTML = tp.strings;
+    }
+});
